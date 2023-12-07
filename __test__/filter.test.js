e1ca771b1f2bbc80fc7e306da18a866e7ed6d0c9
Paavo Jyrkiäinen <paavo.s.jyrkiainen@tuni.fi>
2023-12-07 23:16:01 +0200

    remove commented broken tests in filter.test.js

--- __test__/filter.test.js
+++ __test__/filter.test.js
@@ -4,28 +4,5 @@ describe('filter function', () => {
   test('placeholder', () => {
     expect(true).toBe(true);
   });
-  /*test('filters an array based on a predicate', () => {
-    const users = [
-      { 'user': 'barney', 'active': true },
-      { 'user': 'fred', 'active': false }
-    ];
-
-    const activeUsers = filter(users, ({ active }) => active);
-    expect(activeUsers).toEqual([{ 'user': 'barney', 'active': true }]);
-  });
-
-  test('returns an empty array if no elements match the predicate', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const filteredNumbers = filter(numbers, (num) => num > 10);
-
-    expect(filteredNumbers).toEqual([]);
-  });
-
-  test('returns a new array, not the original one', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const filteredNumbers = filter(numbers, (num) => num % 2 === 0);
-
-    expect(filteredNumbers).toEqual([2, 4]);
-    expect(filteredNumbers).not.toBe(numbers);
-  });*/
+  //TODO: Add tests, filter.js might be broken
 });
