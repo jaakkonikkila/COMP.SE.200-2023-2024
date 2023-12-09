6dacd51e0081744795482f41a584aefca1bd6d3a
Paavo Jyrkiäinen <paavo.s.jyrkiainen@tuni.fi>
2023-12-09 06:29:42 +0200

    implement unit tests for filter.js

--- __test__/filter.test.js
+++ __test__/filter.test.js
@@ -1,8 +1,34 @@
 import filter from '../src/filter.js';
 
 describe('filter function', () => {
-  test('placeholder', () => {
-    expect(true).toBe(true);
+  test('filters elements based on a predicate', () => {
+    const users = [
+      { 'user': 'barney', 'active': true },
+      { 'user': 'fred', 'active': false },
+      { 'user': 'alice', 'active': true },
+    ];
+
+    // Example predicate: select active users
+    const activeUsers = filter(users, ({ active }) => active);
+    
+    expect(activeUsers).toEqual([
+      { 'user': 'barney', 'active': true },
+      { 'user': 'alice', 'active': true },
+    ]);
   });
-  //TODO: Add tests, filter.js might be broken
+
+  test('returns an empty array when no elements match the predicate', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const filteredNumbers = filter(numbers, (num) => num > 10);
+
+    expect(filteredNumbers).toEqual([[]]);
+  });
+
+  test('returns a new array, not the original one', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const filteredNumbers = filter(numbers, (num) => num % 2 === 0);
+
+    expect(filteredNumbers).toEqual([2, 4]);
+    expect(filteredNumbers).not.toBe(numbers);
+  });
 });
