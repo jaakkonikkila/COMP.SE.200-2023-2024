3fcb27ae4ca35442857291f1ab58071fac0ea0d0
Paavo Jyrkiäinen <paavo.s.jyrkiainen@tuni.fi>
2023-12-09 06:35:17 +0200

    implement additional unit tests for isEmpty.js for better coverage

--- __test__/isEmpty.test.js
+++ __test__/isEmpty.test.js
@@ -36,4 +36,37 @@ describe('isEmpty function', () => {
   test('returns true for an empty object', () => {
     expect(isEmpty({})).toBe(true);
   });
+
+  test('returns true for an empty map', () => {
+    expect(isEmpty(new Map())).toBe(true);
+  });
+
+  test('returns true for an empty set', () => {
+    expect(isEmpty(new Set())).toBe(true);
+  });
+
+  test('returns true for an empty typed array', () => {
+    expect(isEmpty(new Uint8Array())).toBe(true);
+  });
+
+  test('returns false for a non-empty map', () => {
+    const map = new Map();
+    map.set('key', 'value');
+    expect(isEmpty(map)).toBe(false);
+  });
+
+  test('returns false for a non-empty set', () => {
+    const set = new Set();
+    set.add('element');
+    expect(isEmpty(set)).toBe(false);
+  });
+
+  test('returns false for a non-empty typed array', () => {
+    const typedArray = new Uint8Array([1, 2, 3]);
+    expect(isEmpty(typedArray)).toBe(false);
+  });
+
+  test('returns false for a non-empty string with only whitespace', () => {
+    expect(isEmpty('   ')).toBe(false);
+  });
 });
